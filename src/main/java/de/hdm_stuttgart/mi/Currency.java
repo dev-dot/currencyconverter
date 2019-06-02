fa806ddbe3792848eeae27fa022846f31b2d32ab
Anton Richter <ar140@hdm-stuttgart.de>
2019-06-02 16:34:40 +0200

    Add Classes, Currency and Rates

---
+++ src/main/java/de/hdm_stuttgart/mi/Currency.java
@@ -0,0 +1,50 @@
+package de.hdm_stuttgart;
+
+
+
+public class Currency {
+
+    private String base;
+    private String date;
+    private float time_last_updated;
+    Rates RatesObject;
+
+
+    // Getter Methods
+
+    public String getBase() {
+        return base;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public float getTime_last_updated() {
+        return time_last_updated;
+    }
+
+    public Rates getRates() {
+        return RatesObject;
+    }
+
+    // Setter Methods
+
+    public void setBase(String base) {
+        this.base = base;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public void setTime_last_updated(float time_last_updated) {
+        this.time_last_updated = time_last_updated;
+    }
+
+    public void setRates(Rates ratesObject) {
+        this.RatesObject = ratesObject;
+    }
+
+
+}
---
+++ src/main/java/de/hdm_stuttgart/mi/Rates.java
@@ -0,0 +1,477 @@
+package de.hdm_stuttgart;
+
+public class Rates {
+ private float EUR;
+ private float AED;
+ private float ARS;
+ private float AUD;
+ private float BGN;
+ private float BRL;
+ private float BSD;
+ private float CAD;
+ private float CHF;
+ private float CLP;
+ private float CNY;
+ private float COP;
+ private float CZK;
+ private float DKK;
+ private float DOP;
+ private float EGP;
+ private float FJD;
+ private float GBP;
+ private float GTQ;
+ private float HKD;
+ private float HRK;
+ private float HUF;
+ private float IDR;
+ private float ILS;
+ private float INR;
+ private float ISK;
+ private float JPY;
+ private float KRW;
+ private float KZT;
+ private float MXN;
+ private float MYR;
+ private float NOK;
+ private float NZD;
+ private float PAB;
+ private float PEN;
+ private float PHP;
+ private float PKR;
+ private float PLN;
+ private float PYG;
+ private float RON;
+ private float RUB;
+ private float SAR;
+ private float SEK;
+ private float SGD;
+ private float THB;
+ private float TRY;
+ private float TWD;
+ private float UAH;
+ private float USD;
+ private float UYU;
+ private float VND;
+ private float ZAR;
+
+
+ // Getter Methods
+
+ public float getEUR() {
+  return EUR;
+ }
+
+ public float getAED() {
+  return AED;
+ }
+
+ public float getARS() {
+  return ARS;
+ }
+
+ public float getAUD() {
+  return AUD;
+ }
+
+ public float getBGN() {
+  return BGN;
+ }
+
+ public float getBRL() {
+  return BRL;
+ }
+
+ public float getBSD() {
+  return BSD;
+ }
+
+ public float getCAD() {
+  return CAD;
+ }
+
+ public float getCHF() {
+  return CHF;
+ }
+
+ public float getCLP() {
+  return CLP;
+ }
+
+ public float getCNY() {
+  return CNY;
+ }
+
+ public float getCOP() {
+  return COP;
+ }
+
+ public float getCZK() {
+  return CZK;
+ }
+
+ public float getDKK() {
+  return DKK;
+ }
+
+ public float getDOP() {
+  return DOP;
+ }
+
+ public float getEGP() {
+  return EGP;
+ }
+
+ public float getFJD() {
+  return FJD;
+ }
+
+ public float getGBP() {
+  return GBP;
+ }
+
+ public float getGTQ() {
+  return GTQ;
+ }
+
+ public float getHKD() {
+  return HKD;
+ }
+
+ public float getHRK() {
+  return HRK;
+ }
+
+ public float getHUF() {
+  return HUF;
+ }
+
+ public float getIDR() {
+  return IDR;
+ }
+
+ public float getILS() {
+  return ILS;
+ }
+
+ public float getINR() {
+  return INR;
+ }
+
+ public float getISK() {
+  return ISK;
+ }
+
+ public float getJPY() {
+  return JPY;
+ }
+
+ public float getKRW() {
+  return KRW;
+ }
+
+ public float getKZT() {
+  return KZT;
+ }
+
+ public float getMXN() {
+  return MXN;
+ }
+
+ public float getMYR() {
+  return MYR;
+ }
+
+ public float getNOK() {
+  return NOK;
+ }
+
+ public float getNZD() {
+  return NZD;
+ }
+
+ public float getPAB() {
+  return PAB;
+ }
+
+ public float getPEN() {
+  return PEN;
+ }
+
+ public float getPHP() {
+  return PHP;
+ }
+
+ public float getPKR() {
+  return PKR;
+ }
+
+ public float getPLN() {
+  return PLN;
+ }
+
+ public float getPYG() {
+  return PYG;
+ }
+
+ public float getRON() {
+  return RON;
+ }
+
+ public float getRUB() {
+  return RUB;
+ }
+
+ public float getSAR() {
+  return SAR;
+ }
+
+ public float getSEK() {
+  return SEK;
+ }
+
+ public float getSGD() {
+  return SGD;
+ }
+
+ public float getTHB() {
+  return THB;
+ }
+
+ public float getTRY() {
+  return TRY;
+ }
+
+ public float getTWD() {
+  return TWD;
+ }
+
+ public float getUAH() {
+  return UAH;
+ }
+
+ public float getUSD() {
+  return USD;
+ }
+
+ public float getUYU() {
+  return UYU;
+ }
+
+ public float getVND() {
+  return VND;
+ }
+
+ public float getZAR() {
+  return ZAR;
+ }
+
+ // Setter Methods
+
+ public void setEUR(float EUR) {
+  this.EUR = EUR;
+ }
+
+ public void setAED(float AED) {
+  this.AED = AED;
+ }
+
+ public void setARS(float ARS) {
+  this.ARS = ARS;
+ }
+
+ public void setAUD(float AUD) {
+  this.AUD = AUD;
+ }
+
+ public void setBGN(float BGN) {
+  this.BGN = BGN;
+ }
+
+ public void setBRL(float BRL) {
+  this.BRL = BRL;
+ }
+
+ public void setBSD(float BSD) {
+  this.BSD = BSD;
+ }
+
+ public void setCAD(float CAD) {
+  this.CAD = CAD;
+ }
+
+ public void setCHF(float CHF) {
+  this.CHF = CHF;
+ }
+
+ public void setCLP(float CLP) {
+  this.CLP = CLP;
+ }
+
+ public void setCNY(float CNY) {
+  this.CNY = CNY;
+ }
+
+ public void setCOP(float COP) {
+  this.COP = COP;
+ }
+
+ public void setCZK(float CZK) {
+  this.CZK = CZK;
+ }
+
+ public void setDKK(float DKK) {
+  this.DKK = DKK;
+ }
+
+ public void setDOP(float DOP) {
+  this.DOP = DOP;
+ }
+
+ public void setEGP(float EGP) {
+  this.EGP = EGP;
+ }
+
+ public void setFJD(float FJD) {
+  this.FJD = FJD;
+ }
+
+ public void setGBP(float GBP) {
+  this.GBP = GBP;
+ }
+
+ public void setGTQ(float GTQ) {
+  this.GTQ = GTQ;
+ }
+
+ public void setHKD(float HKD) {
+  this.HKD = HKD;
+ }
+
+ public void setHRK(float HRK) {
+  this.HRK = HRK;
+ }
+
+ public void setHUF(float HUF) {
+  this.HUF = HUF;
+ }
+
+ public void setIDR(float IDR) {
+  this.IDR = IDR;
+ }
+
+ public void setILS(float ILS) {
+  this.ILS = ILS;
+ }
+
+ public void setINR(float INR) {
+  this.INR = INR;
+ }
+
+ public void setISK(float ISK) {
+  this.ISK = ISK;
+ }
+
+ public void setJPY(float JPY) {
+  this.JPY = JPY;
+ }
+
+ public void setKRW(float KRW) {
+  this.KRW = KRW;
+ }
+
+ public void setKZT(float KZT) {
+  this.KZT = KZT;
+ }
+
+ public void setMXN(float MXN) {
+  this.MXN = MXN;
+ }
+
+ public void setMYR(float MYR) {
+  this.MYR = MYR;
+ }
+
+ public void setNOK(float NOK) {
+  this.NOK = NOK;
+ }
+
+ public void setNZD(float NZD) {
+  this.NZD = NZD;
+ }
+
+ public void setPAB(float PAB) {
+  this.PAB = PAB;
+ }
+
+ public void setPEN(float PEN) {
+  this.PEN = PEN;
+ }
+
+ public void setPHP(float PHP) {
+  this.PHP = PHP;
+ }
+
+ public void setPKR(float PKR) {
+  this.PKR = PKR;
+ }
+
+ public void setPLN(float PLN) {
+  this.PLN = PLN;
+ }
+
+ public void setPYG(float PYG) {
+  this.PYG = PYG;
+ }
+
+ public void setRON(float RON) {
+  this.RON = RON;
+ }
+
+ public void setRUB(float RUB) {
+  this.RUB = RUB;
+ }
+
+ public void setSAR(float SAR) {
+  this.SAR = SAR;
+ }
+
+ public void setSEK(float SEK) {
+  this.SEK = SEK;
+ }
+
+ public void setSGD(float SGD) {
+  this.SGD = SGD;
+ }
+
+ public void setTHB(float THB) {
+  this.THB = THB;
+ }
+
+ public void setTRY(float TRY) {
+  this.TRY = TRY;
+ }
+
+ public void setTWD(float TWD) {
+  this.TWD = TWD;
+ }
+
+ public void setUAH(float UAH) {
+  this.UAH = UAH;
+ }
+
+ public void setUSD(float USD) {
+  this.USD = USD;
+ }
+
+ public void setUYU(float UYU) {
+  this.UYU = UYU;
+ }
+
+ public void setVND(float VND) {
+  this.VND = VND;
+ }
+
+ public void setZAR(float ZAR) {
+  this.ZAR = ZAR;
+ }
+}
